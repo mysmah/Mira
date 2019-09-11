170527db65dc323039ccff1d526147a1e80ae535
mysmah <40302184+mysmah@users.noreply.github.com>
2019-09-12 01:39:49 +0700

    Update BotIO.py

--- BotIO.py
+++ BotIO.py
@@ -13,7 +13,7 @@ model.fit(100)
 async def start(arg):
     #Функция при запуске
     await bot.send_message(-1001184868284, "Сеть инициализирована")
-def on_close(arg):
+async def on_close(arg):
     print("Процесс умирает, нетб))9)")
     r = requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=-1001184868284&text=%D0%9F%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%20%D0%B1%D0%BE%D1%82%D0%B0%20%D0%B7%D0%B0%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BB%D1%81%D1%8F")
 bot = Bot(token=token)
