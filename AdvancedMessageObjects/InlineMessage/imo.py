d54e4036b515c5349f8f51308263dbc3189f6470
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-22 12:00:55 +0300

    Update imo.py
    Шуу

--- AdvancedMessageObjects/InlineMessage/imo.py
+++ AdvancedMessageObjects/InlineMessage/imo.py
@@ -286,7 +286,7 @@ class Module:
 			return None
 		r = requests.get('https://api.telegram.org/bot'+token+'/editMessageText?chat_id='+str(self.msg.chat.id)+'&message_id='+str(self.msg.message_id)+'&text=%D0%92%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9%20%D0%BC%D0%BE%D0%BC%D0%B5%D0%BD%D1%82%20%D0%B1%D0%BE%D1%82%20%D0%BE%D1%82%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%2C%20%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8%20%D0%BD%D0%B5%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%8E%D1%82')
 		with open('runtime.bytes', 'wb') as save:
-                        self.model = None
+			self.model = None
 			self.bot =None
 			pickle.dump(self, save, -1)
 	async def run(self):
