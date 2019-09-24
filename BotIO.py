3d2d5b48e6ba2c1ee0a5febff670a6214f034b11
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-24 19:20:23 +0300

    Update BotIO.py

--- BotIO.py
+++ BotIO.py
@@ -157,9 +157,9 @@ async def nya(message: types.Message):
             text = model.pred(text[5:])
             length = len(text)
             await asyncio.sleep(0.3)
-            while length > 5:
+            while length > 25:
                 await m.chat.do('typing')
-                length -=5
+                length -=25
                 await asyncio.sleep(5)
             await message.chat.do('typing')
             await asyncio.sleep(0.2*length)
