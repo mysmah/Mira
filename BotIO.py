beb37f4b5bc8a05e377f11cb4ce14e14857aaadf
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-25 01:05:47 +0300

    Update BotIO.py
    Du

--- BotIO.py
+++ BotIO.py
@@ -41,13 +41,13 @@ def loadconf():
     if confs[0] == 'NCM':
     	NCMusePretxt = confs[1]
 async def on_close(arg):
-    print("Процесс умирает, нетб))9)")
-    r = requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=-1001184868284&text=%D0%9F%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%20%D0%B1%D0%BE%D1%82%D0%B0%20%D0%B7%D0%B0%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BB%D1%81%D1%8F")
-    imo.shtdw()
     chats[0] = '@catgirl_channel'
     with open('chat.db', 'w') as hui:
         for i in chats:
             hui.write('\n' + i)
+    print("Процесс умирает, нетб))9)")
+    r = requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=-1001184868284&text=%D0%9F%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%20%D0%B1%D0%BE%D1%82%D0%B0%20%D0%B7%D0%B0%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BB%D1%81%D1%8F")
+    imo.shtdw()
 bot = Bot(token=token)
 dp = Dispatcher(bot)
 
