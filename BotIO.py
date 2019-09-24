38f848d5581e53d735b914a94929db3803dc9785
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-25 01:13:24 +0300

    Update BotIO.py

--- BotIO.py
+++ BotIO.py
@@ -21,6 +21,7 @@ def addchat(id: int):
         pass
     else:
         chats.append(id)
+        await bot.send_message('@catgirl_channel', 'a')
 
 async def start(arg):
     #Функция при запуске
@@ -29,8 +30,8 @@ async def start(arg):
     NCMusePretxt = confs.NCMup
     await imo.initof()
     try:
-        with open('chat.db', 'r') as hui:
-            chats = hui.read().split('\n')[1:]
+        with open('chat.db', 'r') as huy:
+            chats = huy.read().split('\n')[1:]
             await bot.send_message('@catgirl_channel',chats)
     except Exception:
         pass
@@ -41,10 +42,10 @@ def loadconf():
     if confs[0] == 'NCM':
     	NCMusePretxt = confs[1]
 async def on_close(arg):
-    chats[0] = '@catgirl_channel'
     with open('chat.db', 'w') as hui:
         for i in chats:
             hui.write('\n' + i)
+            await bot.send_message('@catgirl_channel', i)
     print("Процесс умирает, нетб))9)")
     r = requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=-1001184868284&text=%D0%9F%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81%20%D0%B1%D0%BE%D1%82%D0%B0%20%D0%B7%D0%B0%D0%B2%D0%B5%D1%80%D1%88%D0%B8%D0%BB%D1%81%D1%8F")
     imo.shtdw()
