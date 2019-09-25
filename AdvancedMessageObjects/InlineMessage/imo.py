281631e8b653830954b666ac1b6525788bf85d56
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-25 04:21:27 +0300

    Update imo.py

--- AdvancedMessageObjects/InlineMessage/imo.py
+++ AdvancedMessageObjects/InlineMessage/imo.py
@@ -285,6 +285,7 @@ class Module:
 				
 	def sleep(self):
 		if self.type == 'settings':
+			warn('хуй')
 			r = requests.get('https://api.telegram.org/bot'+token+'/messageDelete?chat_id='+str(self.msg.chat.id)+'&message_id='+str(self.msg.message_id))
 			return None
 		r = requests.get('https://api.telegram.org/bot'+token+'/editMessageText?chat_id='+str(self.msg.chat.id)+'&message_id='+str(self.msg.message_id)+'&text=%D0%92%20%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9%20%D0%BC%D0%BE%D0%BC%D0%B5%D0%BD%D1%82%20%D0%B1%D0%BE%D1%82%20%D0%BE%D1%82%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%2C%20%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8%20%D0%BD%D0%B5%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%8E%D1%82')
