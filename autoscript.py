57450f9ca30db86005c9cfefdbe21656fabdacf4
Cat in Packet <42083774+PacketCat@users.noreply.github.com>
2019-09-29 16:53:31 +0300

    Update autoscript.py

--- autoscript.py
+++ autoscript.py
@@ -6,6 +6,7 @@ from warnings import warn
 chats = []
 
 def startAS(m: Message, act: str = 'reset'):
+	warn(1)
 	if m.chat not in chats:
 		chats.append(AutoScript(m.chat))
 	else:
@@ -16,15 +17,19 @@ def startAS(m: Message, act: str = 'reset'):
 
 class AutoScript:
 	def __init__(self, c: Chat):
+		warn(c)
 		self.chat = c
 		self.unm = 0
+		self.unmt = 0
 	def tick(self, act):
 		if act == 'reset':
 			self.unm = 0
 			self.unmt = time()
+			warn('reset')
 			return None
 		elif act == 'tick':
 			self.unm += 1
+			warn(str(self.unm) + str(self.unmt))
 			if self.unm > 1 and self.unmt + 21.0 < time:
 				return self.chat.id
 				
