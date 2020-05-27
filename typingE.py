bbcd3ef38620698532f76cab4471a5315f145317
mismah <40302184+mysmah@users.noreply.github.com>
2020-05-27 19:13:50 +0700

    Update typingE.py

--- typingE.py
+++ typingE.py
@@ -3,13 +3,13 @@ import asyncio
 
 async def typing(text, message, answer = False):
     length = len(text)
-    await asyncio.sleep(0.3)
+    #await asyncio.sleep(0.3)
     while length > 26:
-        await message.chat.do('typing')
+        #await message.chat.do('typing')
         length -=26
-        await asyncio.sleep(5)
-    await message.chat.do('typing')
-    await asyncio.sleep(0.18*length)
+        #await asyncio.sleep(5)
+    #await message.chat.do('typing')
+    #await asyncio.sleep(0.18*length)
     if answer:
         return await message.answer(text)
     else:
